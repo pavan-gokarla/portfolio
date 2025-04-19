5a5218e1eb36e4c46906d8e89fc9ca2e7f31ae67
pavangokarlag@gmail.com <pavangokarlag@gmail.com>
2025-04-19 22:46:14 +0530

    rectified eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-import { Menu, MenuItem } from "@/components/ui/navbar-menu";
-import { cn } from "@/lib/utils";
+import React from "react";
 import { HeroSection } from "@/components/ui/Hero-Section";
 import { Timeline } from "@/components/ui/timeline";
 import Card, { CardProps } from "@/components/ui/Card";
+import { Navbar } from "./Navbar";
 
 const Home = () => {
     const makeCard = (cardData: CardProps[]) => {
@@ -71,39 +70,3 @@ const Home = () => {
 };
 
 export default Home;
-
-export function Navbar({ className }: { className?: string }) {
-    const [active, setActive] = useState<string | null>(null);
-    return (
-        <div
-            className={cn(
-                "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
-                className
-            )}
-        >
-            <Menu setActive={setActive}>
-                <MenuItem setActive={setActive} active={active} item="Home" />
-                <MenuItem
-                    setActive={setActive}
-                    active={active}
-                    item="Journey"
-                />
-                <MenuItem
-                    setActive={setActive}
-                    active={active}
-                    item="Projects"
-                ></MenuItem>
-                <MenuItem
-                    setActive={setActive}
-                    active={active}
-                    item="About Me"
-                ></MenuItem>
-                <MenuItem
-                    setActive={setActive}
-                    active={active}
-                    item="Contact Me"
-                ></MenuItem>
-            </Menu>
-        </div>
-    );
-}
--- components/ui/Hero-Section.tsx
+++ components/ui/Hero-Section.tsx
@@ -5,7 +5,7 @@ import { motion } from "motion/react";
 import HeroButton from "./Hero-Button";
 import { Button } from "./button";
 import { Github, Linkedin, Twitter } from "lucide-react";
-import { Navbar } from "@/app/page";
+import { Navbar } from "@/app/Navbar";
 
 interface IWords {
     text: string;
